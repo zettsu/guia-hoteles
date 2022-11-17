9fbf99eea1bd9a8e2515c1cf1ca5c806720bacd9
Matias Olivera <jmatias.olivera@gmail.com>
2022-11-16 23:19:45 -0300

    agregado algo de jquery
    agregado algo de jquery

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,47 @@
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
+popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
 });
 
-var myCarousel = document.querySelector('#carousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
+let myCarousel = document.querySelector('#carousel');
+new bootstrap.Carousel(myCarousel, {
     interval: 4000,
     wrap: false
-})
+});
+
+let modalContact = $("#contact");
+let btnContact = $("#btnContact");
+
+modalContact.on('show.bs.modal', function (e){
+    console.log("shown modal, instance method show is called, not visible");
+    if (!btnContact.prop("disabled")) {
+        btnContact.prop("disabled", true);
+    }
+    if (!btnContact.hasClass("btn-primary")) {
+        btnContact.removeClass("btn-primary");
+    }
+    if (!btnContact.hasClass("btn-danger")) {
+        btnContact.addClass("btn-danger");
+    }
+});
+
+modalContact.on('hide.bs.modal', function (e){
+    console.log("hide modal, instance event hide is called, visible");
+});
+
+modalContact.on('shown.bs.modal', function (e){
+    console.log("shown modal, modal is visible");
+});
+
+modalContact.on('hidden.bs.modal', function (e){
+    console.log("hidden modal, modal is successfully hidden(not visible)");
+    if (btnContact.prop("disabled")) {
+        btnContact.prop("disabled", false);
+    }
+    if (!btnContact.hasClass("btn-primary")) {
+        btnContact.addClass("btn-primary");
+    }
+    if (btnContact.hasClass("btn-danger")) {
+        btnContact.removeClass("btn-danger");
+    }
+});
